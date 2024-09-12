8c3e8e9a8257c3669df9899bb5529826deff8900
KevinLi19 <kevin.li@georgiancollege.ca>
2024-09-11 20:25:14 -0400

    Working codes.

--- src/main/java/com/example/demo/CreateMyTreasureController.java
+++ src/main/java/com/example/demo/CreateMyTreasureController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class CreateMyTreasureController {
     @FXML
@@ -19,8 +20,15 @@ public class CreateMyTreasureController {
     @FXML
     private TextField txtName;
 
+    @FXML
+    private Label lblTreasure;
+
 
     public void CreateNewTreasure(ActionEvent actionEvent) {
-
+        MyTreasure myTreasure = new MyTreasure();
+        myTreasure.setName(txtName.getText());
+        myTreasure.setLocation(txtLocation.getText());
+        myTreasure.setComment(txtComment.getText());
+        lblTreasure.setText(myTreasure.toString());
     }
 }
